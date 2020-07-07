16f127c7753c7893fdfab79bf56d29ce462b3fa5
Mohit <mohityd10@gmail.com>
2020-07-07 23:21:15 +0530

    Updates

---
+++ precache-manifest.f7b7f5be00c0d62b72f5d483745fdee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78208f56d5554f1f9633914850d80104",
+    "url": "/pic-some-app/index.html"
+  },
+  {
+    "revision": "2c42f28891903389421b",
+    "url": "/pic-some-app/static/css/main.3ef7dfb9.chunk.css"
+  },
+  {
+    "revision": "5d8577a2125739a4dad4",
+    "url": "/pic-some-app/static/js/2.93b24173.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pic-some-app/static/js/2.93b24173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c42f28891903389421b",
+    "url": "/pic-some-app/static/js/main.66184784.chunk.js"
+  },
+  {
+    "revision": "59dc57b9fe9be8165d69",
+    "url": "/pic-some-app/static/js/runtime-main.c6532be5.js"
+  }
+]);
